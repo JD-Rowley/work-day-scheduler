1d868d6523c6801a72e5dabfad142a2c4537b0db
JD Rowley <jdrowley1989@gmail.com>
2021-08-19 16:11:10 -0600

    add styles in line with mock up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 var currentDayEl = document.getElementById("currentDay");
 
 currentDayEl.textContent = moment().format("dddd, MMM Do");
+
+// color code time blocks to indicate past, present, or future
+var time = moment();
+
+if (time.isAfter(9)) {
+    $("9:00:00").addClass("past");
+}
