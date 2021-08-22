fa826d0cc007335f0d35851ae72d08b457fdbf94
JD Rowley <jdrowley1989@gmail.com>
2021-08-22 17:21:08 -0600

    add text area tags to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,5 @@ var currentDayEl = document.getElementById("currentDay");
 currentDayEl.textContent = moment().format("dddd, MMM Do");
 
 // color code time blocks to indicate past, present, or future
-var time = moment();
-
-if (time.isAfter(9)) {
-    $("9:00:00").addClass("past");
-}
+var currentTime = moment();
+var currentHour = moment().hour();
